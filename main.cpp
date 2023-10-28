34ac2c1ff95ac793af36d5c1c81ee3f17f290a41
Tim Jammer <tim.jammer@sc.tu-darmstadt.de>
2023-10-28 23:16:12 +0200

    added function to print deck

--- main.cpp
+++ main.cpp
@@ -5,24 +5,25 @@
 
 #include <algorithm>
 #include <random>
+#include <iomanip>
 
-class Card{
+class Card {
 public:
     const unsigned int value;
 
     // max value is inclusive
-    static const unsigned int MAX_VALUE=80;
+    static const unsigned int MAX_VALUE = 80;
 
     // special value
-    static const unsigned int START=100;
+    static const unsigned int START = 100;
 
-    static const unsigned int FINISH=101;
-
+    static const unsigned int FINISH = 101;
 
 
     Card(unsigned int v) : value(v) {};
+
     // not copy able
-    Card(const Card&) = delete;
+    Card(const Card &) = delete;
 
     // default destructor
 
@@ -33,31 +34,110 @@ const unsigned int Card::START;
 const unsigned int Card::FINISH;
 
 
-std::vector<std::unique_ptr<Card>> create_deck(){
-        std::vector<std::unique_ptr<Card>> deck;
+std::vector<std::unique_ptr<Card>> create_deck() {
+    std::vector<std::unique_ptr<Card>> deck;
 
-        deck.reserve(100);
+    deck.reserve(100);
     for (int i = 0; i < 5; ++i) {
         deck.push_back(std::make_unique<Card>(Card::START));
         deck.push_back(std::make_unique<Card>(Card::FINISH));
     }
-    for (unsigned int i = 0+1; i < Card::MAX_VALUE+1; ++i) {
+    for (unsigned int i = 0 + 1; i < Card::MAX_VALUE + 1; ++i) {
         deck.push_back(std::make_unique<Card>(i));
     }
 
-        return deck;
+    return deck;
+
+};
+
+class Player {
+public:
+    Player(Player &) = delete;
+
+    std::vector<std::unique_ptr<Card>> draw;
+    std::vector<std::unique_ptr<Card>> discard;
+    std::vector<std::unique_ptr<Card>> hand;
+
+    virtual void make_turn();
+
+};
+
+class PlayArea {
+public:
+    PlayArea() {
+        for (int i = 0; i < 6; ++i) {
+            area.push_back(std::vector<std::unique_ptr<Card>>(6));
+            // nullptr is default
+        }
+    }
+
+    PlayArea(PlayArea &) = delete;
+
+    std::vector<std::vector<std::unique_ptr<Card>>> area;
+
+
+    bool has_start = false;
+    bool has_finish = false;
+
+    void print() const {
+        // first row
+        if (has_start) {
+            std::cout << " S";
+        } else {
+            std::cout << "  ";
+        }
+        for (int i = 0; i < 6; ++i) {
+            std::cout << "\t" << std::setw(2) << i;
+        }
+        std::cout << "\n";
+
+        for (int i = 0; i < 6; ++i) {
+            std::cout << std::setw(2) << i;
+            for (int j = 0; j < 6; ++j) {
+                if (area[i][j]) {
+                    std::cout << "\t" << std::setw(2) << area[i][j]->value;
+                } else {
+                    std::cout << "\t" << "  ";
+                }
+            }
+            std::cout << "\t" << std::setw(2) << i;
+            std::cout << "\n";
+
+        }
+
+        std::cout << "  ";
+        for (int i = 0; i < 6; ++i) {
+            std::cout << "\t" << std::setw(2) << i;
+        }
+        if (has_finish) {
+            std::cout << "\t" << " F";
+        } else {
+            std::cout << "\t" << "  ";
+        }
+        std::cout << "\n";
+    }
 
 };
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    auto rng = std::default_random_engine {};
+    auto rng = std::default_random_engine{};
 
-    auto deck=create_deck();
+    auto deck = create_deck();
 
     std::shuffle(std::begin(deck), std::end(deck), rng);
 
+    PlayArea area = PlayArea();
+
+    area.area[2][3]= std::move(deck.back());
+    deck.pop_back();
+    area.area[2][4]= std::move(deck.back());
+    deck.pop_back();
+    area.area[3][3]= std::move(deck.back());
+    deck.pop_back();
+
+    area.print();
 
     return 0;
 
